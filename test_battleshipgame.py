dea45c9e73f8589b9211f10a80fd884eafba474a
vinothk-master <vinothkann97@gmail.com>
2025-01-02 00:51:46 +0000

    Adding new lines

--- test_battleshipgame.py
+++ test_battleshipgame.py
@@ -3,6 +3,6 @@ from game_engine import Testing_GridA # Import GridA from the appropriate module
 def test_checking_GridA():
     tester  = Testing_GridA()
     tester.ship_add("1")
-    print("Executing the line")
+    print("Executing the test line")
     assert tester.ship_size() == 1
 
